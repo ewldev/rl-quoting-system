810131bafdba7a24878e484cd9e46f0cda5a880e
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-12-07 16:31:24 -0500

    new selections and script.js

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-const form = document.getElementById('form');
+// const form = document.getElementById('form');
 // const messageContainer = document.querySelector('.message-container');
 // const message = document.getElementById('message');
 // let isValid = false;
 
-function validateForm() {
-  // Use HTML constraint API to check form validity
-  isValid = form.checkValidity();
-  // If the form isn't valid
-  if (!isValid) {
-    // Style main message for an error
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
-    return;
-  }
- 
-  // If form is valid and passwords match
-  if (isValid) {
-    // Style main message for success
-    message.textContent = 'Successfully Registered!';
-    message.style.color = 'green';
-    messageContainer.style.borderColor = 'green';
+function showHide(elem) {
+  if(elem.selectedIndex != 0) {
+       //hide the divs
+       for(var i=0; i < divsO.length; i++) {
+           divsO[i].style.display = 'none';
+      }
+      //unhide the selected div
+      document.getElementById('div'+elem.value).style.display = 'block';
   }
 }
 
-  function storeFormData() {
-    const user = {
-      email: form.email.value,
-      business: form.business.value,
-      ownership: form.ownership.value,
-      transactions: form.transactions.value,
-      services: form.services.value
-    };
-    // Do something with user data
-    console.log(user);
-  }
+window.onload=function() {
+  //get the divs to show/hide
+  divsO = document.getElementById("form").getElementsByTagName('div');
+}
+
+  // function storeFormData() {
+  //   const user = {
+  //     email: form.email.value,
+  //     business: form.business.value,
+  //     ownership: form.ownership.value,
+  //     transactions: form.transactions.value,
+  //     services: form.services.value
+  //   };
+  //   // Do something with user data
+  //   console.log(user);
+  // }
   
-  function processFormData(e) {
-    e.preventDefault();
-       storeFormData();      
-  }
+  // function processFormData(e) {
+  //   e.preventDefault();
+  //      storeFormData();      
+  // }
 
   // function processFormData(e) {
   //   e.preventDefault();
@@ -52,79 +47,79 @@ function validateForm() {
   // }
 
   // Event Listener
-  form.addEventListener('submit', processFormData); 
-  form.addEventListener('submit', calculate2); 
+//   form.addEventListener('submit', processFormData); 
+//   form.addEventListener('submit', calculate2); 
 
 
 
-function calculate() {
-  const business = document.getElementById('business-select').value;
-  const ownership = document.getElementById('ownership-select').value;
-  const result = document.getElementById('result');
+// function calculate() {
+//   const business = document.getElementById('business-select').value;
+//   const ownership = document.getElementById('ownership-select').value;
+//   const result = document.getElementById('result');
 
-  if (business == 'retail') {
-    businessRate = 1;
-  } else if (business == 'restaurant') {
-    businessRate = 2;
-  } else {
-    businessRate = 3;
-  }
-  if (ownership == 'sole-proprietorship') {
-    ownershipRate = 10;
-  } else if (ownership == 'partnership') {
-    ownershipRate = 20;
-  } else {
-    ownershipRate = 30;
-  }
-  result.value = businessRate * ownershipRate;
-  console.log(business);
-  console.log(businessRate);
-  console.log(ownership);
-  console.log(ownershipRate);
-}  
+//   if (business == 'retail') {
+//     businessRate = 1;
+//   } else if (business == 'restaurant') {
+//     businessRate = 2;
+//   } else {
+//     businessRate = 3;
+//   }
+//   if (ownership == 'sole-proprietorship') {
+//     ownershipRate = 10;
+//   } else if (ownership == 'partnership') {
+//     ownershipRate = 20;
+//   } else {
+//     ownershipRate = 30;
+//   }
+//   result.value = businessRate * ownershipRate;
+//   console.log(business);
+//   console.log(businessRate);
+//   console.log(ownership);
+//   console.log(ownershipRate);
+// }  
 
 
-function calculate2() {
-  const business = document.getElementById('business-select').value;
-  const ownership = document.getElementById('ownership-select').value;  
-  const result = document.getElementById('result');
+// function calculate2() {
+//   const business = document.getElementById('business-select').value;
+//   const ownership = document.getElementById('ownership-select').value;  
+//   const result = document.getElementById('result');
   
-  switch(business) {
-    case "retail":
-      businessRate = 1;
-      break;
-    case "restaurant":
-      businessRate = 2;
-      break;
-    case "it-consulting":
-      businessRate = 3;
-  }
-  switch(ownership) {
-    case "sole-proprietorship":
-      ownershipRate = 10;
-      break;
-    case "partnership":
-      ownershipRate = 20;
-      break;
-    case "corporation":
-      ownershipRate = 30; 
-  }  
-  result.value = businessRate * ownershipRate;
-  console.log(business);
-  console.log(businessRate);
-  console.log(ownership);
-  console.log(ownershipRate);  
-  }
+//   switch(business) {
+//     case "retail":
+//       businessRate = 1;
+//       break;
+//     case "restaurant":
+//       businessRate = 2;
+//       break;
+//     case "it-consulting":
+//       businessRate = 3;
+//   }
+//   switch(ownership) {
+//     case "sole-proprietorship":
+//       ownershipRate = 10;
+//       break;
+//     case "partnership":
+//       ownershipRate = 20;
+//       break;
+//     case "corporation":
+//       ownershipRate = 30; 
+//   }  
+//   result.value = businessRate * ownershipRate;
+//   console.log(business);
+//   console.log(businessRate);
+//   console.log(ownership);
+//   console.log(ownershipRate);  
+//   }
 
 
 // close button
-window.onload = function(){
-  document.getElementById('close').onclick = function(){
-      this.parentNode.parentNode
-      .removeChild(this.parentNode);
-      return false;
-  };
-};
+// window.onload = function(){
+//   document.getElementById('close').onclick = function(){
+//       this.parentNode.parentNode
+//       .removeChild(this.parentNode);
+//       return false;
+//   };
+// };
   
 
 
