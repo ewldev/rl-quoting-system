9a41fddaf30954250c436ef28dbaab5bac53b805
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-12-09 16:52:38 -0500

    script only works for 150+

--- script.js
+++ script.js
@@ -5,46 +5,41 @@ const form = document.getElementById('form');
 const divs = document.getElementById('form').getElementsByTagName('div');
 const result = document.getElementById('result');
 
-
-function showHide1(elem) {
+function showHide1(elem1) {
   //get the divs to show/hide  
-  console.log('elem.selectedIndex', elem.selectedIndex);  
-  if(elem.selectedIndex != 0) {
+  console.log('elem1.selectedIndex', elem1.selectedIndex);  
+  if(elem1.selectedIndex != 0) {
       //  hide unrelated divs
        for(let i=0; i < divs.length; i++) {
-          divs[i].style.display = 'none';
-          console.log('divs[i]',divs[i]);
+          divs[i].style.display = 'none';          
       }
       //unhide the selected div
-      document.getElementById('div'+elem.value).style.display = 'flex';
-      console.log('elem.value', elem.value)
+      document.getElementById('div'+elem1.value).style.display = 'flex';
+      console.log('elem1.value', elem1.value)
   }
 }
 
-function showHide2(elem) {
-  console.log('elem.selectedIndex', elem.selectedIndex);
-  if(elem.selectedIndex != 0) {
+function showHide2(elem2) {
+  console.log('elem2.selectedIndex', elem2.selectedIndex);
+  if(elem2.selectedIndex != 0) {
     for(let i=1; i < divs.length; i++) {
-      console.log('i',i);
         divs[i].style.display = 'none';
-        console.log('divs[i]',divs[i]);
+        
     }
-    document.getElementById('div'+elem.value).style.display = 'flex';
-    console.log('elem.value', elem.value)
+    document.getElementById('div'+elem2.value).style.display = 'flex';
+    console.log('elem.value', elem2.value)
   }
 }
 
-function showHide3(elem) {
-  console.log('elem.selectedIndex', elem.selectedIndex);
-  if(elem.selectedIndex != 0) {
+function showHide3(elem3) {
+  console.log('elem3.selectedIndex', elem3.selectedIndex);
+  console.log('this',this);
+  if(elem3.selectedIndex != 0) {
     for(let i=2; i < divs.length; i++) {
-      console.log('i',i);
         divs[i].style.display = 'none';
-        console.log('divs[i]',divs[i]);
-    }
-    document.getElementById('div'+elem.value).style.display = 'flex';
-    console.log('elem.value', elem.value);
-    result.value = [];
+    }    
+    document.getElementById('div'+elem3.value).style.display = 'flex';
+    console.log('elem3.value', elem3.value);
     console.log('result.value', result.value);
   }
 }
@@ -76,8 +71,12 @@ function showHide3(elem) {
     const fiftyTxn = document.getElementById('50txn-select').value;
     const oneFiftyTxn = document.getElementById('150txn-select').value;  
     const oneFiftyPlusTxn = document.getElementById('150+txn-select').value;  
+    let serviceRate = 0;
     
-    
+    console.log('result.value', result.value);
+    console.log ('serviceRate',serviceRate);
+    console.log ('serviceRate',serviceRate);
+    console.log ('serviceRate',serviceRate);
 
     switch(fiftyTxn) {      
       // case '':
@@ -85,79 +84,84 @@ function showHide3(elem) {
       // result.value = serviceRate1;  
       //   break;
       case 'consulting-business1':
-        serviceRate1 = '1500';
-        result.value = serviceRate1;  
+        serviceRate = 1500;
+        result.value = serviceRate;  
         break;
       case 'holdings-company1':
-        serviceRate1 = 1700; 
-        result.value = serviceRate1;  
+        serviceRate = 1700; 
+        result.value = serviceRate;  
     }
     switch(oneFiftyTxn) {
-      case 'consulting-business':
-        serviceRate2 = 1800;
-        result.value = serviceRate2; 
+      case 'consulting-business2':
+        serviceRate = 1800;
+        result.value = serviceRate; 
         break;
-      case 'holdings-company':
-        serviceRate2 = 2000;
-        result.value = serviceRate2; 
+      case 'holdings-company2':
+        serviceRate = 2000;
+        result.value = serviceRate; 
         break;
-      case 'trading-retail':
-        serviceRate2 = 2500;
-        result.value = serviceRate2;  
+      case 'trading-retail2':
+        serviceRate = 2500;
+        result.value = serviceRate;  
         break;
-      case 'mortgage-incurance-broker':
-        serviceRate2 = 1800;
-        result.value = serviceRate2; 
+      case 'mortgage-incurance-broker2':
+        serviceRate = 1800;
+        result.value = serviceRate; 
         break;
-      case 'home-renovation-contractor':
-        serviceRate2 = 2300;
-        result.value = serviceRate2; 
+      case 'home-renovation-contractor2':
+        serviceRate = 2300;
+        result.value = serviceRate; 
         break;
-      case 'medical-professional-corporation':
-        serviceRate2 = 2500;
-        result.value = serviceRate2  
+      case 'medical-professional-corporation2':
+        serviceRate = 2500;
+        result.value = serviceRate  
     }  
     switch(oneFiftyPlusTxn) {
-      case 'consulting-business':
-        serviceRate3 = 2500;
-        result.value = serviceRate3; 
+      case 'consulting-business3':
+        serviceRate = 2500;
+        result.value = serviceRate; 
         break;
-      case 'holdings-company':
-        serviceRate3 = 2700;
-        result.value = serviceRate3; 
+      case 'holdings-company3':
+        serviceRate = 2700;
+        result.value = serviceRate; 
         break;
-      case 'trading-retail':
-        serviceRate3 = 3200; 
-        result.value = serviceRate3; 
+      case 'trading-retail3':
+        serviceRate = 3200; 
+        result.value = serviceRate; 
         break;
-      case 'manufacturing':
-        serviceRate3 = 3500;
-        result.value = serviceRate3; 
+      case 'manufacturing3':
+        serviceRate = 3500;
+        result.value = serviceRate; 
         break;
-      case 'restaurant':
-        serviceRate3 = 3500;
-        result.value = serviceRate3; 
+      case 'restaurant3':
+        serviceRate = 3500;
+        result.value = serviceRate; 
         break;  
-      case 'mortgage-incurance-broker':        
-        serviceRate3 = 2500;
-        result.value = serviceRate3; 
+      case 'mortgage-incurance-broker3':        
+        serviceRate = 2500;
+        result.value = serviceRate; 
         break;
-      case 'home-renovation-contractor':
-        serviceRate3 = 3000;
-        result.value = serviceRate3; 
+      case 'home-renovation-contractor3':
+        serviceRate = 3000;
+        result.value = serviceRate; 
         break;
-      case 'medical-professional-corporation':
-        serviceRate3 = 3500;
-        result.value = serviceRate3  
-    }  
-
+      case 'medical-professional-corporation3':
+        serviceRate = 3500;
+        result.value = serviceRate  
+    } 
+    console.log('result.value', result.value); 
+    console.log ('serviceRate',serviceRate);
+    console.log ('serviceRate',serviceRate);
+    console.log ('serviceRate',serviceRate);
+  }  
+    
     
     
     // console.log('serviceRate',serviceRate);
     // console.log(businessRate);
     // console.log(ownership);
     // console.log(ownershipRate);  
-    }
+    
 
 // function calculate() {
 //   const business = document.getElementById('business-select').value;
